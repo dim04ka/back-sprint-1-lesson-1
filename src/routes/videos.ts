6ffb58ddbcf6cbf90b5e9a4e483096a17dae2e30
dim04ka <dmitry.suhotsky@gmail.com>
2026-01-06 09:39:39 +0300

    added some routes

--- src/routes/videos.ts
+++ src/routes/videos.ts
@@ -18,6 +18,12 @@ videosRouter.get('/:id', (req: Request, res: Response) => {
 })
 
 videosRouter.post('', (req: Request, res: Response) => {
+    const date = new Date()
+
+    const createdAt = date.toISOString()
+    const publicationDate = new Date(
+        date.setHours(date.getHours() + 1)
+    ).toISOString()
     const newVideo: Video = {
         id: db.videos.length
             ? db.videos[db.videos.length - 1].id + 1
@@ -27,11 +33,44 @@ videosRouter.post('', (req: Request, res: Response) => {
         availableResolutions: req.body.availableResolutions,
         canBeDownloaded: false,
         minAgeRestriction: null,
-        createdAt: new Date().toISOString(),
-        publicationDate: new Date().toISOString(),
+        createdAt,
+        publicationDate,
     }
 
     db.videos.push(newVideo)
 
     res.status(201).send(newVideo)
+})
+
+videosRouter.put('/:id', (req: Request, res: Response) => {
+    const video = db.videos.find((v) => v.id === +req.params.id)
+    if (!video) {
+        return res.status(404).send({ message: 'Video not found' })
+    }
+
+    const date = new Date()
+
+    const publicationDate = new Date(
+        date.setHours(date.getHours() + 1)
+    ).toISOString()
+
+    video.title = req.body.title
+    video.author = req.body.author
+    video.availableResolutions = req.body.availableResolutions
+    video.canBeDownloaded = req.body.canBeDownloaded ?? false
+    video.minAgeRestriction = req.body.minAgeRestriction ?? null
+    video.publicationDate =
+        req.body.publicationDate ?? publicationDate
+
+    res.status(204).send()
+})
+
+videosRouter.delete('/:id', (req: Request, res: Response) => {
+    const video = db.videos.find((v) => v.id === +req.params.id)
+    if (!video) {
+        return res.status(404).send({ message: 'Video not found' })
+    }
+
+    db.videos = db.videos.filter((v) => v.id !== +req.params.id)
+    res.status(204).send()
 })
