75d807ad1f51eca5e60a8ca182020084419cec66
dim04ka <dmitry.suhotsky@gmail.com>
2026-01-19 16:44:29 +0300

    fix 2

--- src/posts/routes/handlers/get-post.handler.ts
+++ src/posts/routes/handlers/get-post.handler.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
 import { postsRepository } from '../../repository'
 import { HttpStatus } from '../../../core/types/http-statuses'
+import { blogsRepository } from '../../../blogs/repository'
+import { postViewModelMapper } from '../../mapper'
+import { PostViewModel } from '../../dto'
 
 export const getPostHandler = async (req: Request, res: Response) => {
     const id = req.params.id
@@ -10,5 +13,13 @@ export const getPostHandler = async (req: Request, res: Response) => {
             .status(HttpStatus.NotFound)
             .send({ message: 'Post not found' })
     }
-    res.status(HttpStatus.Ok).send(post)
+    const blog = await blogsRepository.findById(post.blogId)
+    if (!blog) {
+        return res
+            .status(HttpStatus.NotFound)
+            .send({ message: 'Blog not found' })
+    }
+    const postViewModel: PostViewModel = postViewModelMapper(post, blog)
+    console.log('postViewModel', postViewModel)
+    res.status(HttpStatus.Ok).send(postViewModel)
 }
